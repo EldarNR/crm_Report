9dbd27e4697ab66944833f86bc28335f34104901
Eldar <eldar121202@gmail.com>
2024-07-04 14:17:43 +0500

    fix supabase #2

--- state/index.ts
+++ state/index.ts
@@ -18,7 +18,7 @@ const supabaseUrl = "https://ustowvbylqottnlzxouw.supabase.co" as string;
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzdG93dmJ5bHFvdHRubHp4b3V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4ODg0ODAsImV4cCI6MjAzMjQ2NDQ4MH0.8frIdjCjrZjoXLmdZ_6LaNVdx1OXJJSN2O63qz9Vw60" as string;
 
-export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
+var supabase = createBrowserClient(supabaseUrl, supabaseKey);
 
 export const useStorage = defineStore({
   id: "Main",
