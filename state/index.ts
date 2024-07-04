f6afbe0092d4df8d7c6bc789fa9d005f0a78049a
Eldar <eldar121202@gmail.com>
2024-07-04 14:23:36 +0500

    fix supabase #3

--- state/index.ts
+++ state/index.ts
@@ -14,8 +14,8 @@ interface reports {
   departament: string;
 }
 
-const supabaseUrl = "https://ustowvbylqottnlzxouw.supabase.co" as string;
-const supabaseKey =
+var supabaseUrl = "https://ustowvbylqottnlzxouw.supabase.co" as string;
+var supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzdG93dmJ5bHFvdHRubHp4b3V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4ODg0ODAsImV4cCI6MjAzMjQ2NDQ4MH0.8frIdjCjrZjoXLmdZ_6LaNVdx1OXJJSN2O63qz9Vw60" as string;
 
 var supabase = createBrowserClient(supabaseUrl, supabaseKey);
